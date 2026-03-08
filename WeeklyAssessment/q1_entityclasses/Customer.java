4bbcdf949465d56a201ff5dee2a7b0ad416546fe
Agniva Chakraborty <113366283+AgnivaChak@users.noreply.github.com>
2026-03-08 18:45:06 +0530

    Create Customer.java

---
+++ WeeklyAssessment/q1_entityclasses/Customer.java
@@ -0,0 +1,96 @@
+package entity;
+
+import jakarta.persistence.*;
+import java.time.LocalDate;
+
+@Entity
+@Table(name = "customers")
+public class Customer {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    private String customerName;
+    private String email;
+    private String gender;
+    private long phone;
+
+    private LocalDate registrationDate;
+
+    /*
+     One-to-One mapping:
+     A customer can have one order.
+     Customer is the owning side of the relationship.
+    */
+
+    @OneToOne(cascade = CascadeType.ALL)
+    @JoinColumn(name = "order_id")
+    private Order order;
+
+    public Customer() {}
+
+    public Customer(String customerName, String email, String gender,
+                    long phone, LocalDate registrationDate, Order order) {
+        this.customerName = customerName;
+        this.email = email;
+        this.gender = gender;
+        this.phone = phone;
+        this.registrationDate = registrationDate;
+        this.order = order;
+    }
+
+    // getters and setters
+
+    public int getId() {
+        return id;
+    }
+
+    public String getCustomerName() {
+        return customerName;
+    }
+
+    public void setCustomerName(String customerName) {
+        this.customerName = customerName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public long getPhone() {
+        return phone;
+    }
+
+    public void setPhone(long phone) {
+        this.phone = phone;
+    }
+
+    public LocalDate getRegistrationDate() {
+        return registrationDate;
+    }
+
+    public void setRegistrationDate(LocalDate registrationDate) {
+        this.registrationDate = registrationDate;
+    }
+
+    public Order getOrder() {
+        return order;
+    }
+
+    public void setOrder(Order order) {
+        this.order = order;
+    }
+}
