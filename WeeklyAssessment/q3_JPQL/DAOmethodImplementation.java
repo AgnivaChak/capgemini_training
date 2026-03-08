fce65d276e5abc774226a7b1481664674895a166
Agniva Chakraborty <113366283+AgnivaChak@users.noreply.github.com>
2026-03-08 18:59:33 +0530

    Create DAOmethodImplementation.java

---
+++ WeeklyAssessment/q3_JPQL/DAOmethodImplementation.java
@@ -0,0 +1,18 @@
+public Customer getCustomerByEmail(String email) {
+
+    EntityManager em = emf.createEntityManager();
+
+    TypedQuery<Customer> query =
+            em.createQuery(
+                    "SELECT c FROM Customer c WHERE c.email = :email",
+                    Customer.class
+            );
+
+    query.setParameter("email", email);
+
+    Customer customer = query.getSingleResult();
+
+    em.close();
+
+    return customer;
+}
